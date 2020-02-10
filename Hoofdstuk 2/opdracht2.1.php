b650ef07f01d3734580bd2238224eedc23539896
Emirhan Alakmak <emirhanalakmak@edu-kw1c.nl>
2020-02-10 15:24:20 +0100

    opdracht2.1.php is verbeterd, opdracht2.2.php is gemaakt en gelinkt

--- Hoofdstuk 2/opdracht2.1.php
+++ Hoofdstuk 2/opdracht2.1.php
@@ -6,14 +6,14 @@
  * File: Opdracht 2.1
  */
 ?>
-<link href="../CSS/Style.css" type="text/css" rel="stylesheet">
+<html>
 <body>
 <header>
     <h1><?php echo "Uitwerking van PHP-opdrachten"; ?></h1>
 </header>
-<aside>
     <a href="index.php">Home</a>
     <p><?php echo "Hallo wereld"; ?>
+        <aside>
     <h2>Menu</h2>
     <ul>
         <li>Hoofdstuk 2
@@ -27,59 +27,9 @@
             </ul>
         </li>
     </ul>
-    <ul>
-        <li>Hoofdstuk 3
-            <ul>
-                <li>
-                    <a href="hoofdstuk3/opdracht3.1.php">Opdracht 3.1</a>
-                </li>
-                <li>
-                    <a href="hoofdstuk3/opdracht3.2.php">Opdracht 3.2</a>
-                </li>
-                <li>
-                    <a href="hoofdstuk3/opdracht3.3.php">Opdracht 3.3</a>
-                </li>
-            </ul>
-        </li>
-    </ul>
-    <ul>
-        <li>Hoofdstuk 4
-            <ul>
-                <li>
-                    <a href="hoofdstuk4/opdracht4.1.php">Opdracht 4.1</a>
-                </li>
-                <li>
-                    <a href="hoofdstuk4/opdracht4.2.php">Opdracht 4.2</a>
-                </li>
-                <li>
-                    <a href="hoofdstuk4/opdracht4.3.php">Opdracht 4.3</a>
-                </li>
-                <li>
-                    <a href="hoofdstuk4/opdracht4.4.php">Opdracht 4.4</a>
-                </li>
-            </ul>
-        </li>
-    </ul>
-    <ul>
-        <li>Hoofdstuk 5
-            <ul>
-                <li>
-                    <a href="hoofdstuk5/opdracht5.1.php">Opdracht 5.1</a>
-                </li>
-                <li>
-                    <a href="hoofdstuk5/opdracht5.2.php">Opdracht 5.2</a>
-                </li>
-                <li>
-                    <a href="hoofdstuk5/opdracht5.3.php">Opdracht 5.3</a>
-                </li>
-                <li>
-                    <a href="hoofdstuk5/opdracht5.4.php">Opdracht 5.4</a>
-                </li>
-            </ul>
-        </li>
-    </ul>
 </aside>
 <main id="wrapper">
     <h2>Uitwerkingen</h2>
 </main>
 </body>
+</html>
---
+++ Hoofdstuk 2/opdracht2.2.php
@@ -0,0 +1,34 @@
+<?php
+/**
+ * User: Emirhan
+ * Date: 5/2/2020
+ * Time: 14:11
+ * File: Opdracht 2.2
+ */
+?>
+<a href="index.php">Home</a>
+<?php
+$text1 = "Hallo";
+$text2 = "een makkelijke taal";
+$text3 = "toch zo'n makkelijke taal";
+$text4 = "wat is";
+$text5 = "PHP";
+$text6 = "Nooit gedacht dat";
+$text7 = "De installatie is best ingewikkeld";
+$text8 = "Fijn";
+$text9 = "?";
+$text10 = ".";
+$text11 = ",";
+$text12 = "<br>";
+$text13 = "is";
+$text14 = "Vind je niet";
+?>
+<html>
+<body>
+<h1><?php echo "Taak 2"; ?></h1>
+<p><?php echo "$text1$text11 $text4 $text5 $text3$text10 $text12 $text7$text10 $text8 toch$text9 $text12 $text6 $text5 $text3"; ?></p>
+<h1><?php echo "Taak 3"; ?></h1>
+<p><?php echo "$text1$text11 $text12 $text8 toch dat $text5 zo'n makkelijke taal $text13$text10 $text12 $text7$text10 $text14$text9"; ?></p>
+</body>
+</html>
+
